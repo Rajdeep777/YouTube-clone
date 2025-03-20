f197c27e7d54be8a3cd7953331c8c55a81adf641
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2025-03-20 19:11:08 +0530

    Updating the styling of searchbar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,27 +3,26 @@ const SearchBar = () => {
     <>
       <div className="w2/4">
         <form>
-          <div className="max-w-96 flex border border-gray-500 rounded-3xl p-1 pl-3 text-sm pr-2">
+          <div className="flex">
+          <div className="sm:w-sm md:w-lg lg:w-xl xl:w-2xl flex border border-[#303030] rounded-full rounded-r rounded pl-5 text-md focus-within:border-[#1C62B9]">
             <input
               id="default-search"
-              className="w-full bg-slate-950 text-white border-none outline-none"
+              className="w-full border-none outline-none"
               placeholder="Search"
               required
             />
-            <button className="bg-slate-950 text-gray-400 font-bold py-2 px-4 rounded inline-flex items-center">
+          </div>
+            <button className="bg-[#222222] text-white py-2 px-5 cursor-pointer rounded-l rounded-full inline-flex items-center border-r border-t border-b border-[#303030]">
               <svg
-                className="w-4 h-4"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6 text-white"
               >
                 <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  fill-rule="evenodd"
+                  d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                  clip-rule="evenodd"
                 />
               </svg>
             </button>
