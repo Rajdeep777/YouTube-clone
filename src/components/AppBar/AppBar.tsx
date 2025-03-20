fd947d8d6321d56d7a7a9c4ff5c36ddcd28fae74
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2025-03-20 09:18:28 +0530

    Changing Search bar styles

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,21 +1,15 @@
-import SearchBar from "../SearchBar/SearchBar"
-
+import SearchBar from "../SearchBar/SearchBar";
 const AppBar = () => {
-    return (
-        <>
-            <div className="flex justify-between">
-                <div>
-                    YouTube
-                </div>
-                <div>
-                    <SearchBar/>
-                </div>
-                <div>
-                    Sign In
-                </div>
-            </div>
-        </>
-  )
-}
-
-export default AppBar
+  return (
+    <>
+      <div className="flex justify-between p-3">
+        <div className="text-md inline-flex items-center">YouTube</div>
+        <div>
+          <SearchBar />
+        </div>
+        <div>Sign In</div>
+      </div>
+    </>
+  );
+};
+export default AppBar;
